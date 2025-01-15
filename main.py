b241d81d6f661efcbac0d22d56143d703c2808f4
Hubert <kiszkahub@gmail.com>
2025-01-15 12:05:28 +0100

    dfs bfs recursive

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ from urllib.parse import urljoin, urlparse
 from collections import deque
 
 class RecursiveWebCrawler:
-    def cleanLinks(allLinks, base_url):
+    def clean_links(self, allLinks, start_url):
         cleanedLinks = []
-        base_domain = urlparse(base_url).netloc
+        base_domain = urlparse(start_url).netloc
         for link in allLinks:
             if 'href' not in link.attrs:
                 continue
 
-            val = urljoin(base_url, link['href'])
-            if val.startswith("http") and urlparse(val).netloc == base_domain:
-                cleanedLinks.append(val)
-                print(val)
+            url = urljoin(start_url, link['href'])
+            if url.startswith("http") and urlparse(url).netloc == base_domain:
+                cleanedLinks.append(url)
+                print(url)
         return cleanedLinks
 
-    def crawl_page(url, visited=None):
+    def crawl_page(self, url, visited=None):
         if visited is None:
             visited = set()
         
@@ -30,18 +30,15 @@ class RecursiveWebCrawler:
             response = requests.get(url)
             soup = BeautifulSoup(response.content, 'html.parser')
             allLinks = soup.find_all("a")
-            cleanedLinks = cleanLinks(allLinks, url)
+            cleanedLinks = self.clean_links(allLinks, url)
             for link in cleanedLinks:
                 if link not in visited:
-                    crawl_page(link, visited)
+                    self.crawl_page(link, visited)
 
         except Exception as e:
             print(e)
 
         return visited
-
-    start_url = sys.argv[1]
-    all_visited = crawl_page(start_url)
 
 class BFSWebCrawler:
     def __init__(self, start_url):
@@ -51,3 +48,89 @@ class BFSWebCrawler:
 
     def clean_links(self, soup, current_url):
         cleaned_links = set()
+        for link in soup.find_all('a'):
+            if 'href' not in link.attrs:
+                continue
+
+            url = urljoin(current_url, link['href'])
+            parsed = urlparse(url)
+            if parsed.scheme.startswith('http') and parsed.netloc == self.base_domain:
+                cleaned_links.add(url)
+            
+        return cleaned_links
+    
+    def crawl_page(self):
+        queue = deque([self.start_url])
+        while queue:
+            current_url = queue.popleft()
+            if current_url in self.visited:
+                continue
+
+            self.visited.add(current_url)
+            try:
+                response = requests.get(current_url)
+                if response.status_code != 200:
+                    continue
+                
+                soup = BeautifulSoup(response.content, 'html.parser')
+                new_links = self.clean_links(soup, current_url)
+                for link in new_links:
+                    if link not in self.visited:
+                        queue.append(link)
+                        print(link)
+                        
+            except Exception as e:
+                print(e)
+                continue
+        return self.visited
+    
+class DFSWebCrawler:
+    def __init__(self, start_url):
+        self.start_url = start_url
+        self.visited = set()
+        self.base_domain = urlparse(start_url).netloc
+
+    def clean_links(self, soup, current_url):
+        cleaned_links = set()
+        for link in soup.find_all('a'):
+            if 'href' not in link.attrs:
+                continue
+
+            url = urljoin(current_url, link['href'])
+            parsed = urlparse(url)
+            if parsed.scheme.startswith('http') and parsed.netloc == self.base_domain:
+                cleaned_links.add(url)
+            
+        return cleaned_links
+    
+    def crawl_page(self):
+        stack = [self.start_url]
+        while stack:
+            current_url = stack.pop()
+            if current_url in self.visited:
+                continue
+
+            self.visited.add(current_url)
+            try:
+                response = requests.get(current_url)
+                if response.status_code != 200:
+                    continue
+                
+                soup = BeautifulSoup(response.content, 'html.parser')
+                new_links = self.clean_links(soup, current_url)
+                for link in new_links:
+                    if link not in self.visited:
+                        stack.append(link)
+                        print(link)
+                        
+            except Exception as e:
+                print(e)
+                continue
+        return self.visited
+
+if __name__ == "__main__":
+    start_url = sys.argv[1]
+    crawler = BFSWebCrawler(start_url)
+    all_links = crawler.crawl_page()
+
+
